4902ca00b160ecfd7b3d5d764e3952689f82d148
Ruslan Sukhar <ruslan@sukhar.info>
2013-10-10 23:52:58 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 simpleDeviceOrientation
 =======================
 
-Device orientation handler, that is easy to use
+The most simple way to handle user's device position in JavaScript.
+No more alpha/betta/gamma! Just x and y, which are counted properly based on device position and orientation.
+
+Usage
+-----
+
+**Check ability to use device orientation:**
+
+```javascript
+simpleDeviceOrientation.isAvailable
+```
+
+**Add (bind) simplified device orientation event:**
+
+```javascript
+simpleDeviceOrientation.addEvent(function(x, y){
+	// x and y are being changed in range from 0 to 1
+});
+```
+
+**Remove (unbind) simplified device orientation event:**
+
+```javascript
+simpleDeviceOrientation.removeEvent(fn);
+```
+
+**Remove all events:**
+
+```javascript
+simpleDeviceOrientation.removeEvents();
+```
---
+++ simpleDeviceOrientation.js
@@ -0,0 +1,122 @@
+/**
+ * simpleDeviceOrientation: the simplest way to use 2d projection of user's device position
+ *
+ * @author Ruslan Sukhar (ruslan@sukhar.info)
+ * @link https://github.com/rsukhar/simpleDeviceOrientation
+ * @license MIT License
+ */
+window.simpleDeviceOrientation = {
+
+	/**
+	 * @var {Boolean} Determines if current device supports device orientation and the class can be used
+	 */
+	isAvailable: false,
+
+	/**
+	 * @var {Boolean} Determines if any event is being attached and handled at the moment
+	 */
+	isActive: false,
+
+	/**
+	 * @var {Array} The stack of events
+	 */
+	_events: [],
+
+	init: function()
+	{
+		this.isAvailable = 'DeviceOrientationEvent' in window;
+	},
+
+	/**
+	 *
+	 * @param {Function} event fn(x, y), x and y both vary in range [0;1]
+	 */
+	addEvent: function(event)
+	{
+		if ( ! this.isAvailable) return;
+		this._events.push(event);
+		if ( ! this.isActive) this._start();
+	},
+
+	removeEvent: function(event)
+	{
+		var index = this._events.indexOf(event);
+		if (index === -1) return;
+		this._events.splice(index, 1);
+		if (this._events.length == 0 && this.isActive) this._stop();
+	},
+
+	removeEvents: function()
+	{
+		this._events.length = 0;
+		if (this.isActive) this._stop();
+	},
+
+	/**
+	 * Start device orientation listening
+	 * @private
+	 */
+	_start: function()
+	{
+		window.addEventListener("deviceorientation", this._eventListener);
+		this.isActive = true;
+	},
+
+	/**
+	 * Stop device orientation listening
+	 * @private
+	 */
+	_stop: function()
+	{
+		window.removeEventListener("deviceorientation", this._eventListener);
+		this.isActive = false;
+	},
+
+	/**
+	 * The function that will be attached to deviceorientation event
+	 * @param {Object} e
+	 * @private
+	 */
+	_eventListener: function(e){
+		var gamma = e.gamma,
+			beta = e.beta,
+			x, y;
+		switch (window.orientation){
+			case -90:
+				gamma = Math.max(-45, Math.min(45, gamma - 20));
+				beta = Math.max(-45, Math.min(45, beta));
+				x = (beta + 45) / 90;
+				y = (45 - gamma) / 90;
+				break;
+			case 90:
+				gamma = Math.max(-45, Math.min(45, gamma + 20));
+				beta = Math.max(-45, Math.min(45, beta));
+				x = (45 - beta) / 90;
+				y = (gamma + 45) / 90;
+				break;
+			case 180:
+				gamma = Math.max(-45, Math.min(45, gamma));
+				beta = Math.max(-45, Math.min(45, beta + 20));
+				x = (gamma + 45) / 90;
+				y = (beta + 45) / 90;
+				break;
+			case 0:
+			default:
+				// Upside down
+				if (gamma < -90 || gamma > 90) gamma = Math.abs(e.gamma)/e.gamma * (180 - Math.abs(e.gamma));
+				gamma = Math.max(-45, Math.min(45, gamma));
+				beta = Math.max(-45, Math.min(45, beta - 20));
+				x = (45 - gamma) / 90;
+				y = (45 - beta) / 90;
+				break;
+		}
+
+		for (var i = 0, l = window.simpleDeviceOrientation._events.length; i < l; i++){
+			try {
+				window.simpleDeviceOrientation._events[i].apply(this, [x, y]);
+			} catch(e) {
+				if (window.console && window.console.error) console.error(e);
+			}
+		}
+	}
+};
